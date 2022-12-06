2de25bc74e0235172a1f6357ac103587dc8876de
Kingsley Eneja <kingsley.eneja@getir.com>
2022-12-06 17:55:58 +0100

    fix/conditional-rendering

--- react-advanced-v18/src/tutorial/3-conditional-rendering/2-short-circuit.tsx
+++ react-advanced-v18/src/tutorial/3-conditional-rendering/2-short-circuit.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+// short-circuit evaluation
+// ternary operator
 
-function ShortCircuit() {
+const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
+
   return (
-    <div>ShortCircuit</div>
-  )
-}
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value : {secondValue}</h1> */}
+      {/* {if(){console.log('hello world')}} */}
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default ShortCircuit
+export default ShortCircuit;
