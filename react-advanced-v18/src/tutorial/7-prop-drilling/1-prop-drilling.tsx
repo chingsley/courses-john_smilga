9c2c10de1ac21c384e596701d2565043cd64f6a0
Kingsley Eneja <kingsley.eneja@getir.com>
2022-12-11 18:06:24 +0100

    Props-drilling

--- react-advanced-v18/src/tutorial/7-prop-drilling/1-prop-drilling.tsx
+++ react-advanced-v18/src/tutorial/7-prop-drilling/1-prop-drilling.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { data, Person } from '../../data';
 
 function PropDrilling() {
+  const [people, setPeople] = useState<Person[]>(data);
+
+  const removePerson = (id: number) => {
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id !== id);
+    });
+  };
+
   return (
-    <div>PropDrilling</div>
-  )
+    <section>
+      <div>PropDrilling</div>
+      <List people={people} removePerson={removePerson} />
+    </section>
+  );
 }
 
-export default PropDrilling
+interface ListProps {
+  people: Person[];
+  removePerson: (id: number) => void;
+}
+
+const List: React.FC<ListProps> = ({ people, removePerson }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            person={person}
+            removePerson={removePerson}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+interface SinglePersonProps {
+  person: Person;
+  removePerson: (id: number) => void;
+}
+const SinglePerson: React.FC<SinglePersonProps> = ({ person, removePerson }) => {
+  const { id, name } = person;
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>Remove</button>
+    </div>
+  );
+};
+
+export default PropDrilling;
