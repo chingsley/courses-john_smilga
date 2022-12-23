d550ca05a0f8bdeaaa7b376c19035059726f0d9d
Kingsley Eneja <kingsley.eneja@getir.com>
2022-12-23 15:19:35 +0100

    Implement the menu page

---
+++ react-projects/05-menu/src/Categories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+interface CategoriesProps {
+  categories: string[];
+      filterItems: (category: string) => void;
+}
+const Categories: React.FC<CategoriesProps> = ({ categories, filterItems}) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            className="filter-btn"
+            type="button"
+            key={index}
+            onClick={() => filterItems(category)}
+            >
+            {category}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Categories
