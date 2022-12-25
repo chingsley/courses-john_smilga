d938a8f77b24d60c4c6068b69e3eb8ed2d4145f6
Kingsley Eneja <kingsley.eneja@getir.com>
2022-12-25 15:19:57 +0100

    Implement React tabs

--- express-server/data/index.js
+++ express-server/data/index.js
@@ -2,3 +2,4 @@ export { default as products } from './products.js';
 export { default as propTypes } from './propTypes.js';
 export { default as storeProducts } from './storeProducts.js';
 export { default as tours } from './tours.js';
+export { default as tabs } from './tabs.js';
