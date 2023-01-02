2f4894f9d2a4e4d0d35985d5462ec4b79756d8a6
Kingsley Eneja <kingsley.eneja@getir.com>
2023-01-02 19:37:23 +0100

    Implement hanburger menu with useRef

---
+++ react-projects/11-navbar/src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import App from './App'
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container!) // createRoot(container!) if you use TypeScript
+root.render(<App  />)
