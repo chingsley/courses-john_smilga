fd99396459fca0f54b3c1ae6b3a62bbd3e5ae155
Kingsley Eneja <kingsley.eneja@getir.com>
2023-01-29 15:21:25 +0100

    Implement the single movie page

--- react-projects/21-movie-db/src/useFetch.ts
+++ react-projects/21-movie-db/src/useFetch.ts
@@ -5,14 +5,15 @@ export interface IMovie {
   Poster: string;
   Title: string;
   Year: string;
+  Plot: string;
 }
 
 const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
 const useFetch = (queryParams: string) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // see below for why it makes sense to set the default to true
   const [error, setError] = useState({ show: false, msg: '' });
-  const [data, setData] = useState<IMovie[] | null>(null);
+  const [data, setData] = useState<IMovie | IMovie[] | null>(null);
 
   const fetchMovies = async (url: string) => {
     setIsLoading(true);
@@ -48,3 +49,22 @@ const useFetch = (queryParams: string) => {
 };
 
 export default useFetch;
+
+
+/**
+ * WHY IT MAKES SENCE TO SET DEFAULT VALUE OF isLoading TO TRUE
+ * The order of execution of this useFetch hook is this
+ * 1.  the useState runs with the defualt values: { isLoading: true, data: null, error: { show: false, msg: '' }}
+ * 2.  those default values are returned to the calling components
+ * 3.  the useEffect runs and updates the default values after the api call
+ * 4.  those updated values are returned to the calling components
+ * 
+ * 
+ * In call component, we add the condition:
+ * if(isLoading) return <div className="loaing"></div>
+ * as a result, during step one, when the component renders with the default
+ * values, this condition will be true, and the loader will render.
+ * Therefore, because we try to access data (as movies or movie) AFTER this if
+ * condition is  checked, then we'll not run into errors of trying to map or
+ * destructure a null value (b/c default value of data is null)
+ */
