16f83fc4d5db0aac492be4176c35f49f02d34e39
Kingsley Eneja <kingsley.eneja@getir.com>
2023-02-20 18:38:48 +0100

    Add types for mockup data

--- react-projects/24-react-github-users/src/context/context.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData/mockUser';
-import mockRepos from './mockData/mockRepos';
-import mockFollowers from './mockData/mockFollowers';
-
-const rootUrl = 'https://api.github.com';
--- react-projects/24-react-github-users/src/context/mockData/mockRepos.ts
+++ react-projects/24-react-github-users/src/context/mockData/mockRepos.ts
@@ -1,4 +1,89 @@
-export default [
+import { IGithubUser } from './mockUser';
+
+export interface ILicense {
+  key: string;
+  name: string;
+  spdx_id: string;
+  url: string;
+  node_id: string;
+}
+export interface IRepo {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  owner: IGithubUser;
+  html_url?: string | null;
+  description?: string | null;
+  fork?: boolean | null;
+  url?: string | null;
+  forks_url?: string | null;
+  keys_url?: string | null;
+  collaborators_url?: string | null;
+  teams_url?: string | null;
+  hooks_url?: string | null;
+  issue_events_url?: string | null;
+  events_url?: string | null;
+  assignees_url?: string | null;
+  branches_url?: string | null;
+  tags_url?: string | null;
+  blobs_url?: string | null;
+  git_tags_url?: string | null;
+  git_refs_url?: string | null;
+  trees_url?: string | null;
+  statuses_url?: string | null;
+  languages_url?: string | null;
+  stargazers_url?: string | null;
+  contributors_url?: string | null;
+  subscribers_url?: string | null;
+  subscription_url?: string | null;
+  commits_url?: string | null;
+  git_commits_url?: string | null;
+  comments_url?: string | null;
+  issue_comment_url?: string | null;
+  contents_url?: string | null;
+  compare_url?: string | null;
+  merges_url?: string | null;
+  archive_url?: string | null;
+  downloads_url?: string | null;
+  issues_url?: string | null;
+  pulls_url?: string | null;
+  milestones_url?: string | null;
+  notifications_url?: string | null;
+  labels_url?: string | null;
+  releases_url?: string | null;
+  deployments_url?: string | null;
+  created_at?: string | null;
+  updated_at?: string | null;
+  pushed_at?: string | null;
+  git_url?: string | null;
+  ssh_url?: string | null;
+  clone_url?: string | null;
+  svn_url?: string | null;
+  homepage?: string | null;
+  size?: number;
+  stargazers_count?: number | null;
+  watchers_count?: number | null;
+  language?: string | null;
+  has_issues: boolean;
+  has_projects: boolean;
+  has_downloads: boolean;
+  has_wiki: boolean;
+  has_pages: boolean;
+  forks_count?: number | null;
+  mirror_url?: string | null;
+  archived: boolean;
+  disabled: boolean;
+  open_issues_count?: number | null;
+  license?: ILicense | null;
+  forks: number;
+  open_issues?: number | null;
+  watchers?: number | null;
+  default_branch?: string | null;
+}
+
+const mockRepos: IRepo[] = [
   {
     id: 239164394,
     node_id: "MDEwOlJlcG9zaXRvcnkyMzkxNjQzOTQ=",
