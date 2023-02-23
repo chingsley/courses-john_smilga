772f970b63eca379261af80b8bd3d459f1378a8a
Kingsley Eneja <kingsley.eneja@getir.com>
2023-02-23 17:53:17 +0100

    Implement user stats

--- react-projects/24-react-github-users/src/components/index.ts
+++ react-projects/24-react-github-users/src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Card } from './Card';
 export { default as Followers } from './Followers';
-export { default as Info } from './Info';
+export { default as UserStats } from './UserStats';
 export { default as Navbar } from './Navbar';
 export { default as Repos } from './Repos';
 export { default as Search } from './Search';
