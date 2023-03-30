cd484890df74ab0ca2ba9df84924603fa089b45b
Kingsley Eneja <kingsley.eneja@getir.com>
2023-03-30 18:00:56 +0100

    Create netlify function for checkout

--- react-projects/25-e-commerce/src/pages/CheckoutPage.tsx
+++ react-projects/25-e-commerce/src/pages/CheckoutPage.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+import { UseCartContext } from '../context/cartContext';
+import { Link } from 'react-router-dom';
+import { StripeCheckout, PageHero, Loading } from '../components';
 
 const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+  const cartContext = UseCartContext();
+  if (!cartContext) return <Loading />;
+
+  const { cart } = cartContext;
+
+  return (
+    <main>
+      <PageHero title='checkout' />
+      <Wrapper className='page'>
+        {cart.length < 1 ? (
+          <div className="empty">
+            <h2>Your cart is empty</h2>
+            <Link className="btn" to="/products">fill it</Link>
+          </div>
+        ) : (
+          <StripeCheckout />
+        )}
+      </Wrapper>
+    </main>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }`;
 
 export default CheckoutPage;
