a4bf560d020232a7ba7cb5b4dcea111e0475b756
Kingsley Eneja <kingsley.eneja@getir.com>
2023-05-24 22:01:33 +0100

    Create the CartContainer

--- redux-toolkit-tutorial/src/features/cart/cartSlice.ts
+++ redux-toolkit-tutorial/src/features/cart/cartSlice.ts
@@ -1,18 +1,19 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from '../../app/store';
+import cartItems, { ICartItem } from '../../data/cartItems';
 
 
-export interface CartState {
-  cartItems: string[];
+export interface ICartState {
+  cartItems: ICartItem[];
   itemsCount: number;
   total: number;
   isLoading: boolean;
 }
 
-const initialState: CartState = {
-  cartItems: [],
-  itemsCount: 0,
-  total: 0,
+const initialState: ICartState = {
+  cartItems: cartItems,
+  itemsCount: cartItems.length,
+  total: cartItems.reduce((acc: number, item: ICartItem) => acc + Number(item.price), 0),
   isLoading: true,
 };
 
