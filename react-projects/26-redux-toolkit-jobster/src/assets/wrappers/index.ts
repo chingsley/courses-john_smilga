4e99d4aba14a6fa916ae5518114157eb77b76403
Kingsley Eneja <kingsley.eneja@getir.com>
2023-07-07 00:24:35 +0100

    Implement stats items

--- react-projects/26-redux-toolkit-jobster/src/assets/wrappers/index.ts
+++ react-projects/26-redux-toolkit-jobster/src/assets/wrappers/index.ts
@@ -5,3 +5,4 @@ export { default as SmallSidebarWrapper } from './SmallSidebar';
 export { default as BigSidebarWrapper } from './BigSidebar';
 export { default as NavbarWrapper } from './Navbar';
 export { default as StatsContainerWrapper } from './StatsContainer';
+export { default as StatItemWrapper } from './StatItem';
