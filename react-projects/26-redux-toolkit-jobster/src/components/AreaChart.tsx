2733f6a0c23e24732229c37765affe4781643bdf
Kingsley Eneja <kingsley.eneja@getir.com>
2023-07-08 18:00:12 +0100

    Implement chart items

--- react-projects/26-redux-toolkit-jobster/src/components/AreaChart.tsx
+++ react-projects/26-redux-toolkit-jobster/src/components/AreaChart.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import {
+  ResponsiveContainer,
+  AreaChart as AChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
 
-const AreaChart = () => {
+const AreaChart = ({ data }: { data: any; }) => {
   return (
-    <div>AreaChart</div>
+    <ResponsiveContainer width='100%' height={300}>
+      <AChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type='monotone' dataKey='count' stroke='#1e3a8a' fill='#3b82f6' />
+      </AChart>
+    </ResponsiveContainer>
   );
 };
 
--- react-projects/26-redux-toolkit-jobster/src/components/BarChart.tsx
+++ react-projects/26-redux-toolkit-jobster/src/components/BarChart.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import {
+  BarChart as BChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
-const BarChart = () => {
+const BarChart = ({ data }: { data: any; }) => {
   return (
-    <div>BarChart</div>
+    <ResponsiveContainer width='100%' height={300}>
+      <BChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='10 10 ' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey='count' fill='#3b82f6' barSize={75} />
+      </BChart>
+    </ResponsiveContainer>
   );
 };
 
