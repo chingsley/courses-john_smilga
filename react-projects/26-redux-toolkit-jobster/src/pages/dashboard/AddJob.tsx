61cb4d64369eb2be13d74ca856941315dc458b70
Kingsley Eneja <kingsley.eneja@getir.com>
2023-07-16 20:58:22 +0100

    Imnplement form to add new job

--- react-projects/26-redux-toolkit-jobster/src/pages/dashboard/AddJob.tsx
+++ react-projects/26-redux-toolkit-jobster/src/pages/dashboard/AddJob.tsx
@@ -1,8 +1,137 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { FormRow, FormRowSelect } from '../../components';
+import {
+  handleChange,
+  clearValues,
+  createJob,
+  editJob,
+} from '../../features/job/jobSlice';
+import { AppDispatch, RootState } from '../../app/store';
+import { IJob } from '../../types/jobs';
+import { DashboardFormPageWrapper } from '../../assets/wrappers';
 
 const AddJob = () => {
+  const { user } = useSelector((store: RootState) => store.user);
+  const dispatch: AppDispatch = useDispatch();
+
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId,
+  } = useSelector((store: RootState) => store.job);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      return toast.error('Please fill out all fields');
+    }
+
+    if (isEditing) {
+      dispatch(
+        editJob({
+          jobId: editJobId,
+          job: { position, company, jobLocation, jobType, status } as IJob
+        })
+      );
+      return;
+    }
+    dispatch(createJob({ position, company, jobLocation, jobType, status } as IJob));
+  };
+
+  const handleJobInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    dispatch(handleChange({ name, value }));
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    dispatch(handleChange({ name, value }));
+  };
+
+  useEffect(() => {
+    if (!isEditing) {
+      dispatch(
+        handleChange({
+          name: 'jobLocation',
+          value: user?.location
+        })
+      );
+    }
+  }, []);
+
   return (
-    <div>AddJob</div>
+    <DashboardFormPageWrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+        <div className='form-center'>
+          {/* position */}
+          <FormRow
+            type='text'
+            name='position'
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type='text'
+            name='company'
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* jobLocation */}
+          <FormRow
+            type='text'
+            name='jobLocation'
+            labelText='job location'
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* status */}
+          <FormRowSelect
+            name='status'
+            value={status}
+            handleChange={handleSelectChange}
+            list={statusOptions}
+          />
+          {/* job type*/}
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            value={jobType}
+            handleChange={handleSelectChange}
+            list={jobTypeOptions}
+          />
+          <div className='btn-container'>
+            <button
+              type='button'
+              className='btn btn-block clear-btn'
+              onClick={() => dispatch(clearValues())}
+            >
+              clear
+            </button>
+            <button
+              type='submit'
+              className='btn btn-block submit-btn'
+              disabled={isLoading}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </DashboardFormPageWrapper>
   );
 };
 
